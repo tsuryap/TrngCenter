a024392ee671a2bd86e61c0b810e3f4afec01a34
User1987031987 <user19870319@gmail.com>
2017-09-17 08:56:22 +0530

    today changes

---
+++ app/components/nav/NavCtrl.js
@@ -0,0 +1,17 @@
+app.controller('NavCtrl', ['$scope',function($scope){
+	$scope.appName = 'Online Training Center';
+	$scope.navHeaders = [
+        {
+        	headerName: 'Dashboard',
+        	href:'#!dashboard'
+        },
+        {
+        	headerName: 'Trainings',
+        	href:'#!trng'
+        },
+        {
+        	headerName: 'Admin',
+        	href:'#!admin'
+        }
+	];
+}]);
