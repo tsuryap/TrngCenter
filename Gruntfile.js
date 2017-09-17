4299f7fc8bf64443b576339f47ed5e0fcefe9f08
User1987031987 <user19870319@gmail.com>
2017-09-17 09:08:37 +0530

    test

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n',
+            },
+            jsFiles: {
+                src: [
+                    "bower_components/angular/angular.js",
+                    "bower_components/angular-ui-router/release/angular-ui-router.js",
+                    "app/app.js",
+                    "app/**/*.js"
+                ],
+                dest: 'build/<%= pkg.name %>.js',
+            }
+        }
+    }); 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('default', ['concat']);
+};
