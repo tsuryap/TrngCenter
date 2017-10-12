3d73825697b9ab4a9da5016ef8205d2789633b44
User1987031987 <user19870319@gmail.com>
2017-10-12 08:04:36 +0530

    today changes

--- app/components/trng/TrngCtrl.js
+++ app/components/trng/TrngCtrl.js
@@ -1,9 +1,23 @@
 app.controller('TrngCtrl',['$scope',function($scope){
-	$scope.resetForm = function(){
+	$scope.resetForm = function(form){
 		$scope.trngObj = {};
+		var errorObj = form.$error;
+			for(var key in errorObj) {
+				var array = errorObj[key];
+				for(var i= 0;i< array.length;i++){
+					array[i].$touched = false;
+				}
+		 }
 	};
 	$scope.saveForm = function(form){
 		if(form.$invalid) {
+			var errorObj = form.$error;
+			for(var key in errorObj) {
+				var array = errorObj[key];
+				for(var i= 0;i< array.length;i++){
+					array[i].$touched = true;
+				}
+			}
 			return false;
 		}
 
--- build/trngcenter.js
+++ build/trngcenter.js
@@ -41867,11 +41867,25 @@ app.controller('NavCtrl', ['$scope',function($scope){
 	];
 }]);
 app.controller('TrngCtrl',['$scope',function($scope){
-	$scope.resetForm = function(){
+	$scope.resetForm = function(form){
 		$scope.trngObj = {};
+		var errorObj = form.$error;
+			for(var key in errorObj) {
+				var array = errorObj[key];
+				for(var i= 0;i< array.length;i++){
+					array[i].$touched = false;
+				}
+		 }
 	};
 	$scope.saveForm = function(form){
 		if(form.$invalid) {
+			var errorObj = form.$error;
+			for(var key in errorObj) {
+				var array = errorObj[key];
+				for(var i= 0;i< array.length;i++){
+					array[i].$touched = true;
+				}
+			}
 			return false;
 		}
 
